582d0d8e972a08680f8fe21085da0cdb4250d437
kirstinlyon <kirstin@mltwelve.com>
2024-02-12 18:08:59 +0000

    Separated the scripts for a main flow and other functions.  Added gitignore.

--- Scripts/create_epi_data.R
+++ Scripts/create_epi_data.R
@@ -6,310 +6,93 @@ library(readxl)
 library(tidyverse)
 library(mozR)
 
+#VALUES AND PATHS (Updated by end user) --------------------------------------------------------------
+
+SPECTRUM_PATH <- "Data/indicators.csv"
+MAPPING_PATH  <- "Data/mapping.xlsx"  #TODO:  this needs to point to google
+MER_PATH      <- "Data/Genie_SITE_IM_2023_2024.txt"
+MER_PATH <- "Data/dg_ref.rds"
+MILITARY_PSNU_PATH <- "Data/military_psnu_contribution.xlsx"   #TODO:  this needs to point to google
+START_DATE <- "2024 Q1"
+END_DATE <- "2024 Q1"
+
+#location for epi dataset.  What should the right name be?
+EPI_OUTPUT_PATH <- "Dataout/epi.csv"
+
+#All functions
+source("Scripts/utilities.R")
 
 
+# Create Datasets -----------------------------------------------------------------------
 
-#VALUES AND PATHS --------------------------------------------------------------
+create_epi_data <- function(x){
 
-VAL_QUARTER = "qtr4"
-VAL_YEAR = 2023
-VAL_YEAR_SPECTRUM = 2022
+  VAL_YEAR <- year(x)
+  VAL_QUARTER <- paste0("qtr",quarter(x))
+  VAL_YEAR_SPECTRUM <- VAL_YEAR - 1
 
-SPECTRUM_RAW <- "Data/indicators.csv"
-MAPPING_RAW  <- "Data/mapping.xlsx"
-MER_RAW      <- "Data/dg_ref.rds"
+  #only TX_CURR for military_mozambique
+  mer_military_df <- create_military_MER(MER_PATH, VAL_YEAR, VAL_QUARTER, MILITARY_PSNU_PATH)
 
+  #TX_CURR and TX_PVLS
+  mer_non_military_df <- create_mer(MER_PATH, VAL_YEAR, VAL_QUARTER) %>%
+    filter(snu1 != "_Military Mozambique")
+  mer_all_df <- mer_non_military_df %>%
+    bind_rows(mer_military_df)
 
-folder_period <- glue::glue("{VAL_YEAR}_{VAL_QUARTER}")
+  spectrum_df <- create_spectrum(SPECTRUM_PATH, MAPPING_PATH, VAL_YEAR_SPECTRUM)
 
-#Local path
-path_quarterly_epi_output_file <-
-  glue::glue("Dataout/quarterly/epi_{folder_period}.csv")
-
-
-#historic
-path_historic_epi_output_file <- "Dataout/epi.csv"
-
-
-#LOAD SPECTRUM DATA ------------------------------------------------------------
-
-## End: only 2023, sex = male or female, only detailed age groups, only district, three indicators.
-## include psnuUID and snuuidfor later joining
-mapping <- read_excel(MAPPING_RAW) %>%
-  select(area_id, MER_name, psnuuid, snu1uid, snu1, psnu)
-
-spectrum_df <- read_csv(SPECTRUM_RAW)
-
-military_psnu <- read_xlsx("Data/military_psnu_contribution.xlsx",
-                           sheet = "contribution")
-
-
-# Load MER Data
-mer_df <- read_psd(MER_RAW) %>%
-  filter(fiscal_year == VAL_YEAR,
-         ageasentered != "Unknown Age")%>%
-  select(
-    snu1,
-    snu1uid,
-    psnu,
-    psnuuid,
-    fiscal_year,
-    ageasentered,
-    sex,
-    indicator,
-    numeratordenom,
-    standardizeddisaggregate,
-    all_of(VAL_QUARTER)
-  )
-
-mer_non_military_df <- mer_df %>%
-  filter(snu1 != "_Military Mozambique")
-
-mer_military_df <- mer_df %>%
-  filter(snu1 == "_Military Mozambique") %>%
-  select(-c(psnu, psnuuid, snu1, snu1uid))
-
-
-
-
-#DATA MUNGING ------------------------------------------------------------------
-
-
-# Spectrum Dataset
-spectrum <- spectrum_df %>%
-  left_join(mapping, join_by(area_id)) %>%
-
-  mutate(
-    name = case_when(is.na(MER_name) ~ area_name,
-                     TRUE ~ MER_name),
-    year = year(my(quarter_label)),
-    sex = str_to_title(sex),
-    age_group_label = case_when(
-      age_group_label == "<1" ~ "<01",
-      age_group_label == "1-4" ~ "01-04",
-      age_group_label == "5-9" ~ "05-09",
-      TRUE ~ age_group_label
-    ),
-    age_group_type = case_when(
-      age_group_label %in% c("<01", "01-04", "05-09", "10-14", "<15") ~ "ped",
-      TRUE ~ "adult"
-    )
-  ) %>%
-
-  filter(
-    sex != "Both",
-    indicator %in% c("plhiv", "aware_plhiv_num"),
-    area_level == 3,
-    year == VAL_YEAR_SPECTRUM,
-    age_group_label %in% c(
-      "<01",
-      "01-04",
-      "05-09",
-      "10-14",
-      "15-19",
-      "20-24",
-      "25-29",
-      "30-34",
-      "35-39",
-      "40-44",
-      "45-49",
-      "50-54",
-      "55-59",
-      "60-64",
-      "65+"
-    )
-  ) %>%
-  select(
-    -c(
-      MER_name,
-      area_name,
-      quarter_label,
-      calendar_quarter,
-      median,
-      mode,
-      area_id,
-      area_level_label,
-      age_group,
-      indicator,
-      name,
-      area_level,
-      year,
-      se,
-      upper,
-      lower
-    )
-  ) %>%
-  rename(value = mean,
-         indicator = indicator_label,
-         age = age_group_label)
-
-#MER Dataset
-mer <- mer_non_military_df %>%
-  mutate(
-    psnuuid = case_when(
-      psnu == "Chonguene" ~ "jDWCkBXYllV",
-      psnu == "Mandlakaze" ~ "VjJPal9jqUi",
-      TRUE ~ psnuuid
-    ),
-    age_group_type = case_when(
-      ageasentered %in% c("<01", "01-04", "05-09", "10-14", "<15") ~ "ped",
-      TRUE ~ "adult"
-    )
+  TX_CURR_df   <- mozR::create_epi_model(
+    mer_all_df,
+    "TX_CURR",
+    c("Age/Sex/HIVStatus", "Age Aggregated/Sex/HIVStatus"),
+    num_dem = "N",
+    label = "TX_CURR"
   )
 
 
-#military
-military_df <- military_psnu %>%
-  clean_names() %>%
-  select(-tx_curr) %>%
-  mutate(col_join = "join")
-
-mer_military <- mer_military_df %>%
-  mutate(age_group_type = case_when(
-    ageasentered %in% c("<01", "01-04", "05-09", "10-14", "<15") ~ "ped",
-    .default = "adult"),
-    col_join = "join"
+  TX_PVLS_N_df <- mozR::create_epi_model(
+    mer_all_df,
+    "TX_PVLS",
+    c(
+      "Age/Sex/Indication/HIVStatus",
+      "Age Aggregated/Sex/Indication/HIVStatus",
+      "Age/Sex/HIVStatus",
+      "Age Aggregated/Sex/HIVStatus"
+    ),
+    num_dem = "N",
+    label = "TX_PVLS_N"
   )
 
-
-# CREATE EPI MODEL DATA --------------------------------------------------------
-
-TX_CURR_M <- mer_military %>%
-  filter(indicator == "TX_CURR",
-         numeratordenom == "N",
-         standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "Age Aggregated/Sex/HIVStatus")) %>%
-  group_by(sex,
-           ageasentered,
-           numeratordenom,
-           age_group_type,
-           col_join,
-           standardizeddisaggregate) %>%
-  rename(value = VAL_QUARTER) %>%
-
-
-  #how to summarize correctly by
-  dplyr::summarise(value = sum(value, na.rm = TRUE), .groups = 'drop')
-
-TX_CURR_military_total <- TX_CURR_M %>%
-  summarise(value = sum(value)) %>%
-  pull()
-
-#calculate what the value will be based on the total percentage for TX_CURR
-TX_CURR_military <- TX_CURR_M %>%
-  left_join(military_df, by = "col_join", relationship = "many-to-many") %>%
-  mutate(total_tx_curr = TX_CURR_military_total,
-         sex_age_percent = value / total_tx_curr, na.rm = NULL,
-         tx_curr_psnu = round(total_tx_curr * percent_total, 0),
-         tx_curr_sex_age = round(tx_curr_psnu * sex_age_percent, 0),
-         indicator = "TX_CURR",
-         fiscal_year = VAL_YEAR
-         ) %>%
-
-  select(-c(percent_total, total_tx_curr, value, sex_age_percent, tx_curr_psnu,
-            col_join)) %>%
-rename(!!VAL_QUARTER == tx_curr_sex_age)
-
-#Add military to Mer non-military - only needed for TX_CURR
-
-# use rbind and not union to get all data
-mer_all <- mer %>%
-  rbind(TX_CURR_military)
-
-TX_CURR   <- mozR::create_epi_model(
-  mer_all,
-  "TX_CURR",
-  c("Age/Sex/HIVStatus", "Age Aggregated/Sex/HIVStatus"),
-  num_dem = "N",
-  label = "TX_CURR"
-)
-
-
-TX_PVLS_N <- mozR::create_epi_model(
-  mer,
-  "TX_PVLS",
-  c(
-    "Age/Sex/Indication/HIVStatus",
-    "Age Aggregated/Sex/Indication/HIVStatus",
-    "Age/Sex/HIVStatus",
-    "Age Aggregated/Sex/HIVStatus"
-  ),
-  num_dem = "N",
-  label = "TX_PVLS_N"
-)
-
-TX_PVLS_D <- mozR::create_epi_model(
-  mer,
-  "TX_PVLS",
-  c(
-    "Age/Sex/Indication/HIVStatus",
-    "Age Aggregated/Sex/Indication/HIVStatus",
-    "Age/Sex/HIVStatus",
-    "Age Aggregated/Sex/HIVStatus"
-  ),
-  num_dem = "D",
-  label = "TX_PVLS_D"
-)
-
-# CREATE EPI DATA --------------------------------------------------------------
-
-epi <- spectrum %>%
-  select(-age_group_type) %>%
-  union(TX_CURR) %>%
-  union(TX_PVLS_D) %>%
-  union(TX_PVLS_N) %>%
-  mutate(value = round(value, digits = 0)) %>%
-  pivot_wider(
-    names_from = "indicator",
-    values_from = "value",
-    values_fn = list(values = sum)
-  ) %>%
-  clean_names()  %>%
-  mutate(
-    tx_curr_adj = case_when(
-      (tx_curr >= number_plhiv_aware) ~ number_plhiv_aware,
-      TRUE ~ tx_curr
+  TX_PVLS_D_df <- mozR::create_epi_model(
+    mer_all_df,
+    "TX_PVLS",
+    c(
+      "Age/Sex/Indication/HIVStatus",
+      "Age Aggregated/Sex/Indication/HIVStatus",
+      "Age/Sex/HIVStatus",
+      "Age Aggregated/Sex/HIVStatus"
     ),
-    period      = paste(VAL_YEAR, VAL_QUARTER)
-  ) %>%
+    num_dem = "D",
+    label = "TX_PVLS_D"
+  )
 
-  pivot_longer(
-    cols = c(
-      tx_curr,
-      tx_curr_adj,
-      plhiv,
-      number_plhiv_aware,
-      tx_pvls_n,
-      tx_pvls_d
-    ),
-    names_to = "indicator",
-    values_to = "value"
-  ) %>%
-  mutate(period = stringr::str_replace(period, "qtr", "")) %>%
+  # CREATE EPI DATA --------------------------------------------------------------
+  epi_df <- create_epi_df(spectrum_df, TX_CURR_df, TX_PVLS_D_df, TX_PVLS_N_df, VAL_YEAR, VAL_QUARTER)
 
-  #create the virally suppressed proxy
-  pivot_wider(names_from = indicator, values_from = value) %>%
-  mutate_all(~replace(., is.na(.), 0))  %>%
-  mutate(tx_pvls_percent = case_when(tx_pvls_d == 0 ~ 0,
-                                  .default = tx_pvls_n / tx_pvls_d),
-         virally_suppressed = round(tx_curr * tx_pvls_percent, digits = 0)
-         ) %>%
-  select(-tx_pvls_percent) %>%
-  pivot_longer(cols = 8:14,
-               names_to = "indicator",
-               values_to = "value")
+  return(epi_df)
+
+}
 
 
-# OUTPUT ----------------------------------------------------------------------------
-
-# to disk
-write_excel_csv2(epi, path_quarterly_epi_output_file)
-
-# Create historical datasets
-epi_historic_files <- dir("Dataout/quarterly/", pattern = "*.csv")
-epi_historic <- epi_historic_files %>%
-  map( ~ read_csv2(file.path("Dataout/quarterly/", .))) %>%
-  reduce(rbind) %>%
+epi_df <- map_dfr(create_date(START_DATE, END_DATE), create_epi_data) %>%
   write_excel_csv2("Dataout/epi.csv")
 
+# Check Values ----------------------------------------------------------------------------
+#returns an empty df if no discrepencies exist.  Currently discrepencies due to military
+
+mer_df <- create_mer(MER_PATH, VAL_YEAR, VAL_QUARTER) # original with minimal cleaning - needs to be wrapped in purrr for this to work
+
+check_value_discrepencies <- check_totals(mer_df, epi_df)
 
 
---
+++ Scripts/utilities.R
@@ -0,0 +1,271 @@
+# All functions used in create_epi_data
+
+
+
+create_date <- function(START_DATE, END_DATE){
+    START_DATE_CONVERT <- lubridate::yq(START_DATE)
+    END_DATE_CONVERT <- lubridate::yq(END_DATE)
+    DATE_RANGE <- seq(from = START_DATE_CONVERT, to = END_DATE_CONVERT, by = "quarter")
+
+    # Define names for each element in the vector
+    quarter_names <- paste0("quarter", 1:length(DATE_RANGE))
+
+    # Convert the sequence into a named vector
+    named_date_range <- setNames(DATE_RANGE, quarter_names)
+}
+
+
+
+create_spectrum <- function(spectrum_file, mapping_file, VAL_YEAR_SPECTRUM){
+
+    mapping_temp_df <- read_excel(mapping_file) %>%
+        select(area_id, MER_name, psnuuid, snu1uid, snu1, psnu)
+
+    spectrum_temp_df <- read_csv(spectrum_file) %>%
+        left_join(mapping_temp_df, join_by(area_id)) %>%
+        mutate(
+            name = case_when(is.na(MER_name) ~ area_name,
+                             TRUE ~ MER_name),
+            year = year(my(quarter_label)),
+            sex = str_to_title(sex),
+            age_group_label = case_when(
+                age_group_label == "<1" ~ "<01",
+                age_group_label == "1-4" ~ "01-04",
+                age_group_label == "5-9" ~ "05-09",
+                TRUE ~ age_group_label
+            ),
+            age_group_type = case_when(
+                age_group_label %in% c("<01", "01-04", "05-09", "10-14", "<15") ~ "ped",
+                TRUE ~ "adult"
+            )
+        ) %>%
+
+        filter(
+            sex != "Both",
+            indicator %in% c("plhiv", "aware_plhiv_num"),
+            area_level == 3,
+            year == VAL_YEAR_SPECTRUM,
+            age_group_label %in% c(
+                "<01",
+                "01-04",
+                "05-09",
+                "10-14",
+                "15-19",
+                "20-24",
+                "25-29",
+                "30-34",
+                "35-39",
+                "40-44",
+                "45-49",
+                "50-54",
+                "55-59",
+                "60-64",
+                "65+"
+            )
+        ) %>%
+        select(
+            -c(
+                MER_name,
+                area_name,
+                quarter_label,
+                calendar_quarter,
+                median,
+                mode,
+                area_id,
+                area_level_label,
+                age_group,
+                indicator,
+                name,
+                area_level,
+                year,
+                se,
+                upper,
+                lower
+            )
+        ) %>%
+        rename(value = mean,
+               indicator = indicator_label,
+               age = age_group_label)
+
+    return(spectrum_temp_df)
+}
+
+create_mer <- function(MER_PATH, VAL_YEAR, VAL_QUARTER){
+
+    #call military here to join everything
+
+    temp_df <- read_msd(MER_PATH) %>%
+        filter(fiscal_year == VAL_YEAR,
+               ageasentered != "Unknown Age",
+               indicator %in% c("TX_CURR", "TX_PVLS"),
+               standardizeddisaggregate %in% c("Age/Sex/HIVStatus",
+                                                "Age Aggregated/Sex/HIVStatus",
+                                                "Age/Sex/Indication/HIVStatus",
+                                                "Age Aggregated/Sex/Indication/HIVStatus",
+                                                "Age/Sex/HIVStatus",
+                                                "Age Aggregated/Sex/HIVStatus")
+
+               )%>%
+        select(
+            snu1,
+            snu1uid,
+            psnu,
+            psnuuid,
+            fiscal_year,
+            ageasentered,
+            sex,
+            indicator,
+            numeratordenom,
+            standardizeddisaggregate,
+            all_of(VAL_QUARTER)
+        ) %>%
+        mutate(
+            psnuuid = case_when(
+                psnu == "Chonguene" ~ "jDWCkBXYllV",
+                psnu == "Mandlakaze" ~ "VjJPal9jqUi",
+                TRUE ~ psnuuid
+            ),
+            age_group_type = case_when(
+                ageasentered %in% c("<01", "01-04", "05-09", "10-14", "<15") ~ "ped",
+                TRUE ~ "adult"
+            )
+        ) %>%
+        select(-fiscal_year) %>%
+        rename(value := !!VAL_QUARTER)
+    return(temp_df)
+}
+
+
+create_military_MER <- function(MER_PATH, VAL_YEAR, VAL_QUARTER, MILITARY_PSNU_PATH){
+
+    temp_contribution_df <- read_xlsx(MILITARY_PSNU_PATH,
+                        sheet = "contribution") %>%
+        clean_names() %>%
+        select(-tx_curr) %>%
+        mutate(col_join = "join")
+
+    temp_military <- create_mer(MER_PATH, VAL_YEAR, VAL_QUARTER) %>%
+        filter(snu1 == "_Military Mozambique",
+               indicator == "TX_CURR",
+               numeratordenom == "N",
+               standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "Age Aggregated/Sex/HIVStatus")) %>%
+        select(-c(psnu, psnuuid, snu1, snu1uid)) %>%
+        mutate( col_join = "join"
+        ) %>%
+        group_by(sex,
+                 ageasentered,
+                 indicator,
+                 numeratordenom,
+                 age_group_type,
+                 col_join,
+                 standardizeddisaggregate) %>%
+        rename(military_value = value) %>%
+        #how to summarize correctly by
+        dplyr::summarise(military_value = sum(military_value, na.rm = TRUE), .groups = 'drop')
+
+    TX_CURR_military_total <- temp_military %>%
+        summarise(military_value = sum(military_value)) %>%
+        pull()
+
+
+    #calculate what the value will be based on the total percentage for TX_CURR
+    TX_CURR_military <- temp_military %>%
+        left_join(temp_contribution_df, by = "col_join", relationship = "many-to-many") %>%
+        mutate(total_tx_curr = TX_CURR_military_total,
+               sex_age_percent = military_value / total_tx_curr, na.rm = NULL,
+               tx_curr_psnu = round(total_tx_curr * percent_total, 0),
+               tx_curr_sex_age = round(tx_curr_psnu * sex_age_percent, 0),
+        ) %>%
+
+        select(-c(percent_total, total_tx_curr,military_value, sex_age_percent, tx_curr_psnu,
+                  col_join)) %>%
+        rename(value = tx_curr_sex_age)
+
+    return(TX_CURR_military)
+
+}
+
+
+create_epi_df <- function(spectrum_df, TX_CURR, TX_PVLS_N, TX_PVLS_D, VAL_YEAR, VAL_QUARTER){
+
+    epi_temp <- spectrum_df %>%
+        select(-age_group_type) %>%
+        bind_rows(TX_CURR) %>%
+        bind_rows(TX_PVLS_D) %>%
+        bind_rows(TX_PVLS_N) %>%
+        mutate(value = round(value, digits = 0)) %>%
+        pivot_wider(
+            names_from = "indicator",
+            values_from = "value",
+            values_fn = list(values = sum)
+        ) %>%
+        clean_names()  %>%
+        pivot_longer(
+            cols = c(
+                tx_curr,
+                plhiv,
+                number_plhiv_aware,
+                tx_pvls_n,
+                tx_pvls_d
+            ),
+            names_to = "indicator",
+            values_to = "value"
+        ) %>%
+
+        #create the virally suppressed proxy
+        pivot_wider(names_from = indicator, values_from = value) %>%
+        mutate_all(~replace(., is.na(.), 0))  %>%
+        mutate(tx_pvls_percent = case_when(tx_pvls_d == 0 ~ 0,
+                                           .default = tx_pvls_n / tx_pvls_d),
+               virally_suppressed = round(tx_curr * tx_pvls_percent, digits = 0)
+        ) %>%
+        select(-tx_pvls_percent) %>%
+        pivot_longer(cols = 7:12,
+                     names_to = "indicator",
+                     values_to = "value") %>%
+        mutate(period = paste(VAL_YEAR, VAL_QUARTER) ,
+                period = stringr::str_replace(period, "qtr", ""))
+
+    return(epi_temp)
+
+}
+
+#province or snu level
+check_totals <- function(mer_raw_df, epi_df){
+
+    before <- mer_raw_df  %>%
+        filter(indicator %in% c("TX_CURR", "TX_PVLS")) %>%
+        group_by(indicator, numeratordenom) %>%
+        summarise(value = sum(value, na.rm = TRUE), .groups = "drop" ) %>%
+        mutate(indicator = paste(indicator, numeratordenom, sep = "_")) %>%
+        select(-numeratordenom) %>%
+        pivot_wider(names_from = indicator) %>%
+        rename(TX_CURR = TX_CURR_N) %>%
+        clean_names() %>%
+        rename(tx_curr_before = tx_curr,
+           tx_pvls_n_before = tx_pvls_n,
+           tx_pvls_d_before = tx_pvls_d)
+
+
+    after <- epi_df  %>%
+        filter(indicator %in% c("tx_curr", "tx_pvls_n", "tx_pvls_d")) %>%
+        group_by(indicator) %>%
+        summarise(value = sum(value, na.rm = TRUE), .groups = "drop" ) %>%
+        pivot_wider(names_from = indicator) %>%
+        rename(tx_curr_after = tx_curr,
+               tx_pvls_n_after = tx_pvls_n,
+               tx_pvls_d_after = tx_pvls_d)
+
+    diff <- before %>%
+        cross_join(after) %>%
+        mutate(tx_curr_diff = tx_curr_before - tx_curr_after,
+               tx_pvls_n_diff = tx_pvls_n_before - tx_pvls_n_after,
+               tx_pvls_d_diff = tx_pvls_d_before - tx_pvls_d_after) %>%
+        select(-c(tx_curr_before, tx_curr_after, tx_pvls_n_before, tx_pvls_n_after,
+                  tx_pvls_d_before, tx_pvls_d_after)) %>%
+        #remove when there is no difference
+        filter(tx_curr_diff != 0, tx_pvls_d_diff != 0, tx_pvls_n_diff != 0)
+
+    return (diff)
+}
+
