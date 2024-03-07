357fc8d1f773f68861be65ea6f18b7346a47111a
kirstinlyon <kirstin@mltwelve.com>
2024-03-07 08:48:30 +0100

    Documentation updated create_military_mer updated to include creation of PVLS_N and PVLS_D based on SISMA contribution and age/sex PVLS % for the military

--- Scripts/utilities.R
+++ Scripts/utilities.R
@@ -2,12 +2,12 @@
 
 
 
-#' Title
+#' Create all year-quarters between the start and end date.
 #'
-#' @param START_DATE
-#' @param END_DATE
+#' @param START_DATE Enter the start date for data
+#' @param END_DATE Enter the end date for data
 #'
-#' @return
+#' @return All quarters between the start and end date
 #' @export
 #'
 #' @examples
@@ -21,10 +21,22 @@ create_date <- function(START_DATE, END_DATE){
 
     # Convert the sequence into a named vector
     named_date_range <- setNames(DATE_RANGE, quarter_names)
+
+    return(named_date_range)
 }
 
 
 
+#' Creates the spectrum dataset for use in epi. Only two indicators are needed and only age groups that match MER
+#'
+#' @param spectrum_file Spectrum dataset
+#' @param mapping_file mapping dataset maps the SNU and PSNU between spectrum and MER
+#' @param VAL_YEAR_SPECTRUM spectrum year is one year before the MER year
+#'
+#' @return a clean spectrum dataset with PLHIV, PLHIV aware at a district level for speficied age groups
+#' @export
+#'
+#' @examples
 create_spectrum <- function(spectrum_file, mapping_file, VAL_YEAR_SPECTRUM){
 
     # download from google drive
@@ -105,19 +117,17 @@ create_spectrum <- function(spectrum_file, mapping_file, VAL_YEAR_SPECTRUM){
     return(spectrum_temp_df)
 }
 
-#' Title
+#' Creates a clean MER dataset with only TX_CURR and TX_PVLS indicators
 #'
-#' @param MER_PATH
-#' @param VAL_YEAR
-#' @param VAL_QUARTER
+#' @param MER_PATH Path to the MER dataset
+#' @param VAL_YEAR Year for the data
+#' @param VAL_QUARTER quarter for the data
 #'
-#' @return
+#' @return a cleaned MER dataset for a specific year-quarter.
 #' @export
 #'
 #' @examples
 create_mer <- function(MER_PATH, VAL_YEAR, VAL_QUARTER){
-
-    #call military here to join everything
 
     temp_df <- read_psd(MER_PATH) %>%
         filter(fiscal_year == VAL_YEAR,
@@ -163,77 +173,79 @@ create_mer <- function(MER_PATH, VAL_YEAR, VAL_QUARTER){
 }
 
 
-#' Title
+#' Creates the military dataset using contributions from SISMA
 #'
-#' @param mer_df
-#' @param VAL_YEAR
-#' @param VAL_QUARTER
-#' @param MILITARY_PSNU_PATH
+#' @param mer_df MER dataset
+#' @param VAL_YEAR Year of data
+#' @param VAL_QUARTER quarter of data
+#' @param MILITARY_PSNU_PATH contribution for each PSNU from SISMA
 #'
-#' @return
+#' @return MER data for Military Mozambique based on SISMA contribution
 #' @export
 #'
 #' @examples
 create_military_MER <- function(mer_df, VAL_YEAR, VAL_QUARTER, MILITARY_PSNU_PATH){
 
+    #SISMA file - contribution for each PSNU
     temp_contribution_df <- googlesheets4::read_sheet(MILITARY_PSNU_PATH) %>%
         clean_names() %>%
         select(-tx_curr) %>%
         mutate(col_join = "join")
 
     temp_military <- mer_df %>%
-        filter(snu1 == "_Military Mozambique",
-               indicator == "TX_CURR",
-               numeratordenom == "N",
-               standardizeddisaggregate %in% c("Age/Sex/HIVStatus", "Age Aggregated/Sex/HIVStatus")
-               ) %>%
+        filter(snu1 == "_Military Mozambique") %>%
         select(-c(psnu, psnuuid, snu1, snu1uid)) %>%
-        mutate( col_join = "join"
+        mutate(
+            col_join = "join",
+            indicator = case_when(indicator == "TX_PVLS" ~ paste(indicator, numeratordenom, sep = "_"),
+                                  TRUE ~ indicator)
         ) %>%
-        group_by(sex,
-                 ageasentered,
-                 indicator,
-                 numeratordenom,
-                 age_group_type,
-                 col_join,
-                 standardizeddisaggregate) %>%
         rename(military_value = value) %>%
-    #how to summarize correctly by
-        dplyr::summarise(military_value = sum(military_value, na.rm = TRUE), .groups = 'drop')
+        select(-standardizeddisaggregate) %>%
+        group_by(ageasentered, sex, indicator, age_group_type, col_join) %>%
+        summarise(military_value = sum(military_value, na.rm = TRUE), .groups = "drop") %>%
+        pivot_wider(names_from = indicator, values_from = military_value) %>%
+        filter(TX_CURR > 0 | is.na(TX_CURR) |
+                   TX_PVLS_N > 0 | is.na(TX_PVLS_N) |
+                   TX_PVLS_D > 0 | is.na(TX_PVLS_D)) %>%
 
-     TX_CURR_military_total <- temp_military %>%
-        summarise(military_value = sum(military_value)) %>%
-        pull()
+        #create the PVLS %
+        mutate(pvls_percent = TX_PVLS_N / TX_PVLS_D)
 
-
-    #calculate what the value will be based on the total percentage for TX_CURR
     TX_CURR_military <- temp_military %>%
         left_join(temp_contribution_df, by = "col_join", relationship = "many-to-many") %>%
-        mutate(total_tx_curr = TX_CURR_military_total,
-               sex_age_percent = military_value / total_tx_curr, na.rm = NULL,
-               tx_curr_psnu = round(total_tx_curr * percent_total, 0),
-               tx_curr_sex_age = round(tx_curr_psnu * sex_age_percent, 0),
-        ) %>%
-
-        select(-c(percent_total, total_tx_curr,military_value, sex_age_percent, tx_curr_psnu,
-                  col_join)) %>%
-        rename(value = tx_curr_sex_age)
+        mutate(TX_CURR_new = round(TX_CURR * percent_total,0),
+               TX_PVLS_D_new = round(TX_PVLS_D * percent_total,0),
+               TX_PVLS_N_new = round(pvls_percent * TX_PVLS_D_new,0)
+        )%>%
+        select(-c(TX_PVLS_D, TX_PVLS_N, pvls_percent, col_join, percent_total,
+                  TX_CURR)) %>%
+        rename(TX_CURR = TX_CURR_new,
+               TX_PVLS_N = TX_PVLS_N_new,
+               TX_PVLS_D = TX_PVLS_D_new) %>%
+        pivot_longer(cols = starts_with("TX"),
+                     names_to = "indicator",
+                     values_to = "value") %>%
+        mutate(standarddizeddisaggregate = "military") %>%
+        separate(indicator, into = c("indicator", "numeratordenom"), sep = "(?<=TX_PVLS)_", remove = FALSE) %>%
+        mutate(numeratordenom = case_when(indicator == "TX_CURR" ~ "N",
+                                          .default = numeratordenom))
 
     return(TX_CURR_military)
 
 }
 
 
-#' Title
+#' Creates the final epi dataset by combining spectrum and MER data
 #'
-#' @param spectrum_df
-#' @param TX_CURR
-#' @param TX_PVLS_N
-#' @param TX_PVLS_D
-#' @param VAL_YEAR
-#' @param VAL_QUARTER
+#' @param spectrum_df  Cleaned spectrum dataset
+#' @param TX_CURR TX_CURR model
+#' @param TX_PVLS_N TX_PVLS_N model
+#' @param TX_PVLS_D TX_PVLS_D model
+#' @param VAL_YEAR Year of data
+#' @param VAL_QUARTER quarter of data
 #'
-#' @return
+#' @return the epi dataset
 #' @export
 #'
 #' @examples
